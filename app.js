1006e0f03754c1b6adadb0bc88fa2a0dc5083f6d
amirSohel007 <amirsohel27@gmail.com>
2020-06-01 15:00:18 +0530

    created API

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const shortUrl = require("node-url-shortener");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get("/url-shortner", (req, res) => {
+  shortUrl.short(req.body.url, function (err, url) {
+    if (!err) {
+      res.json({ shortUrl: url });
+    }
+  });
+});
+
+const PORT = 8000 || process.env.PORT;
+app.listen(8000, () => console.log("Server is up and running"));
